feadfdcc04db2ccae700055d0f0a6d1e6206d989
hiu <nguyenvanhieu13102003@gmail.com>
2024-03-10 13:26:36 +0700

    Add project files.

---
+++ DoAnWebBanTaiKhoan/Models/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace DoAnWebBanTaiKhoan.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Menu> Menus { get; set; }
+    }
+}
